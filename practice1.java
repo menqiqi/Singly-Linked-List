89dd916984bdf687173868bfb0e8a8b45b241582
menqiqi <39701053+menqiqi@users.noreply.github.com>
2018-11-24 09:01:51 +0800

    给定一个带有头结点 head 的非空单链表，返回链表的中间结点。

--- practice1.java
+++ practice1.java
@@ -1,37 +1,28 @@
 /**
+ * 给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
+ *
+ * 如果有两个中间结点，则返回第二个中间结点。
+ */
+
+/**
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
  *     ListNode next;
  *     ListNode(int x) { val = x; }
- *
+ * }
  */
 class Solution {
-    public ListNode swapPairs(ListNode head) {
-        if(head == null){
-            return null;
-        }
+    public ListNode middleNode(ListNode head) {
         if(head.next == null){
             return head;
         }
-        ListNode node = head;
-        head = head.next;
-        while(node != null){
-            ListNode res = node.next;
-            if(res != null){
-                ListNode tmp = res.next;
-                if(tmp == null || tmp.next == null){
-                    node.next = tmp;
-                }else{
-                    node.next = tmp.next;
-                }
-                res.next = node;
-                node = tmp;
-            }else{
-                break;
-            }
-
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return head;
+        return slow;
     }
 }
